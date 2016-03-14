cd47910112a64f76d8015858c904849fc8da6b22
pgerth <phg@dainst.de>
2016-03-14 20:07:23 +0100

    delete method added

--- fileHandling.py
+++ fileHandling.py
@@ -8,11 +8,25 @@ def fileList(source, target, query):
     matches = []
     for root, dirs, files in os.walk(source):
         for name in files:
-            if fnmatch.fnmatch(name, query):
+            if fnmatch.fnmatch(name,query):
                 filePath = os.path.join(root, name)
                 os.makedirs(target + os.path.dirname(filePath[len(source):]), exist_ok=True)
                 shutil.copy2(filePath, target + filePath[len(source):])
                 matches.append(filePath)
     return matches
 
-fileList('/Users/user/Documents/Privat/test', '/Users/user/Documents/Privat/new', 'OS*_Cover*.jpg')
+def fileDelete(source, query):
+    matches = []
+    for root, dirs, files in os.walk(source):
+        for name in files:
+            if fnmatch.fnmatch(name,query):
+                filePath = os.path.join(root, name)
+                os.remove(filePath)
+                matches.append(filePath)
+    return matches   
+
+# fileList('/Users/phg/Documents/Privat/test', '/Users/phg/Documents/Privat/new', '*.jpg')
+
+# fileDelete('/Users/phg/Documents/Privat/test','*.jpg')
+
+
