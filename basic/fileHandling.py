0b66e4eee44a2f9ad81b1186282a0b5ed72c669b
phg <philipp.gerth@gmx.net>
2017-05-30 17:23:20 +0200

    restructured

--- basic/fileHandling.py
+++ basic/fileHandling.py
@@ -3,6 +3,7 @@
 import os
 import shutil
 import fnmatch
+import sys, getopt
 
 def fileList(source, target, query):
     matches = []
@@ -23,10 +24,31 @@ def fileDelete(source, query):
                 filePath = os.path.join(root, name)
                 os.remove(filePath)
                 matches.append(filePath)
-    return matches   
+    return matches
 
 # fileList('/Users/phg/Documents/Privat/test', '/Users/phg/Documents/Privat/new', '*.jpg')
 
 # fileDelete('/Users/phg/Documents/Privat/test','*.jpg')
 
+# main function, which takes also care of the parameter handling
+def main(argv):
+    source =
+    target =
+    docs = 'owl2config.py -s <source> -t <target>'
+    try:
+        opts, args = getopt.getopt(argv,"hs:t:",["source=","target="])
+    except getopt.GetoptError:
+        print docs
+        sys.exit(2)
 
+    for opt, arg in opts:
+        if opt == '-h':
+            print docs
+            sys.exit()
+        elif opt in ("-s", "--source"):
+            source = arg
+        elif opt in ("-t", "--target"):
+            target = arg
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
