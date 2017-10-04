1c58b0ae3184fe038a6910f1c1653a3b12d10421
phg <philipp.gerth@gmx.net>
2017-10-04 17:24:02 +0200

    minor changes due to file names

--- basic/filenameMove.sh
+++ basic/filenameMove.sh
@@ -1,18 +1,21 @@
-!#/bin/bash
+!#/usr/bin/env bash
 # simple bash helper script for hanisch piana archive
-# it searches for files and moves them dependend on their filename
+# it searchs for files and moves them dependend on their filename
 # e.g. file: "D-DAI-HP-K12-z-001.jpg"
-# script moves the file into "./Kasten 12/Zeichnung/"
+# moves the file into "./Kasten 12/Zeichnung/"
 
 find . -name "D-DAI*" | while read fname; do
-  directory=$(echo $fname | cut -d"-" -f 6)
-  type=$(echo $fname | cut -d"-" -f 5)
+  echo $fname
+  directory=$(echo $fname | cut -d"-" -f 7)
+  echo $directory
+  type=$(echo $fname | cut -d"-" -f 6)
+  echo $type
   if [ ${directory:0:1} = "K" ]; then
     directory="${directory//K/Kasten }"
   elif [ ${directory:0:1} = "M" ]; then
     directory="${directory//M/Mappe }"
   fi
-  if [ $type = "f" ]; then
+  if [ $type = "f" ] || [ $type = "n" ]; then
     subdir="Fotos"
   elif [ $type = "z" ]; then
     subdir="Zeichnungen"
