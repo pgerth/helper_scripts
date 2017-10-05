110620acd7a18e3c9699f9b13f5e4e3c4149729e
phg <philipp.gerth@gmx.net>
2017-10-05 09:22:14 +0200

    new mapping for filename attributes

--- basic/filenameMove.sh
+++ basic/filenameMove.sh
@@ -1,4 +1,4 @@
-!#/usr/bin/env bash
+#!/usr/bin/env bash
 # simple bash helper script for hanisch piana archive
 # it searchs for files and moves them dependend on their filename
 # e.g. file: "D-DAI-HP-K12-z-001.jpg"
@@ -15,12 +15,14 @@ find . -name "D-DAI*" | while read fname; do
   elif [ ${directory:0:1} = "M" ]; then
     directory="${directory//M/Mappe }"
   fi
-  if [ $type = "f" ] || [ $type = "n" ]; then
+  if [ $type = "f" ]; then
     subdir="Fotos"
   elif [ $type = "z" ]; then
     subdir="Zeichnungen"
   elif [ $type = "do" ]; then
     subdir="Dokumente"
+  elif [ $type = "n" ]; then
+    subdir="Negative"
   fi
   # check if directory exists, if not create it
   [ -d "$directory" ] || mkdir "$directory"
