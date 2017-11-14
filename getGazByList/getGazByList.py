e7ed8e2f5b9681455299a17357cddc0c91811e82
phg <philipp.gerth@gmx.net>
2017-11-14 13:33:36 +0100

    getGazByList csv basic

--- getGazByList/getGazByList.py
+++ getGazByList/getGazByList.py
@@ -1,7 +1,17 @@
 import json
 import requests
+import csv
 
-gazId = 2086499
-r = requests.get('https://gazetteer.dainst.org/place/' + str(gazId) + '.json')
-data = r.json()
-print(data.get('gazId'))
+def getGazById(gazId):
+    r = requests.get('https://gazetteer.dainst.org/place/' + str(gazId) + '.json')
+    data = r.json()
+    try:
+        print(data['prefLocation']['coordinates'])
+    except KeyError as e:
+        print(e)
+    return
+
+with open('places.csv', 'rt') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        getGazById(row[0])
