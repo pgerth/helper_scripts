ef5e0a3b713b34eb3a5109ff9fd6629a56d0b342
phg <philipp.gerth@gmx.net>
2018-01-02 14:25:44 +0100

    getGazByList - added description

--- getGazByList/getGazByList.py
+++ getGazByList/getGazByList.py
@@ -1,5 +1,19 @@
-# small helper script to extract gazetteer information for a given
-# list of gaz ids
+# author: Philipp Gerth
+#
+# The intention of the script is to enrich a csv with gazetteer Ids
+# (https://gazetteer.dainst.org/) by coordinates.
+#
+# Parameters:
+# -i --input= This parameter hands over the input file name of the csv
+# -c --column= This parameter hands over the column number of the gazetteer Id,
+#  numbering starts with 0!
+# -d --delimiter= This optional parameter is used to define the delimiter of
+#  the input csv file, per default ";" is used!
+#
+# Examples:
+#
+# python getGazByList.py -c 'relative/path/places.csv' -c 0 -d ,
+# python getGazByList.py -c 'relative/path/synode.csv' -c 3
 
 import sys, getopt
 import json
